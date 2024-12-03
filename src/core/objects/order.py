5f3d64d44d664524196e39153fde2706f181e272
kkarnyshov <kyrylo.karnyshov@gmail.com>
2024-12-03 14:39:11 +0200

    Added tests for status codes. Added basic creation of invalid order in order.py

--- src/core/objects/order.py
+++ src/core/objects/order.py
@@ -1,4 +1,5 @@
 from random import randint
+from typing import Any
 from dataclasses import dataclass
 import json
 
@@ -6,7 +7,7 @@ import json
 @dataclass
 class Order:
     order_id: int
-    pet_id: int
+    pet_id: Any
     quantity: int
     shipDate: str
     status: str
@@ -21,6 +22,26 @@ class Order:
         cls.status = "complete"
         cls.complete = bool(randint(0, 1))
 
+# TODO: move as function
+        order_json = {"id": cls.order_id,
+                      "petId": cls.pet_id,
+                      "quantity": cls.quantity,
+                      "shipDate": cls.shipDate,
+                      "status": cls.status,
+                      "complete": cls.complete}
+
+        return json.dumps(order_json)
+
+    @classmethod
+    def generate_invalid_order(cls):
+        cls.order_id = bool(randint(0, 1))
+        cls.pet_id = randint(0, 10)
+        cls.quantity = randint(0, 10)
+        cls.shipDate = "2023-06-26T15:38:10.849Z"
+        cls.status = "complete"
+        cls.complete = bool(randint(0, 1))
+
+# TODO: move as function
         order_json = {"id": cls.order_id,
                       "petId": cls.pet_id,
                       "quantity": cls.quantity,
