7f8c4ab800b670aa660f0ab838a337a2b1206631
Kyrylo Karnyshov <kyrylo.karnyshov@gmail.com>
2024-12-07 19:51:53 +0200

    Added basic tests for status codes for endpoints that support JSON. POST request for image upload and pet update by form data have been omitted.

--- src/services/pet.py
+++ src/services/pet.py
@@ -5,17 +5,24 @@ from requests.api import get, post, put, delete
 
 class PetService:
     pet_url = f"{BaseAPI.base_url}pet/"
+    find_by_status_url = f"{pet_url}findByStatus?status"
     pet = Pet.generate_pet()
+    pet_updated = Pet.generate_updated_pet(pet)
+    pet_updated_invalid_id = Pet.generate_updated_pet_invalid_id()
+    pet_updated_invalid_body = Pet.generate_updated_pet_invalid_body(pet)
     headers = BaseAPI.headers
 
     def get_pet(self, pet_id):
         return get(url=self.pet_url + f"{pet_id}", headers=self.headers)
 
-    def create_pet(self):
-        return post(url=self.pet_url, data=self.pet, headers=self.headers)
+    def find_by_status(self, status):
+        return get(url=self.find_by_status_url + f"{status}", headers=self.headers)
 
-    def update_pet(self, pet_id):
-        return put(url=self.pet_url + f"{pet_id}", data=self.pet, headers=self.headers)
+    def create_pet(self, pet):
+        return post(url=self.pet_url, data=pet, headers=self.headers)
+
+    def update_pet(self, pet_updated):
+        return put(url=self.pet_url, data=pet_updated, headers=self.headers)
 
     def delete_pet(self, pet_id):
         return delete(url=self.pet_url + f"{pet_id}", headers=self.headers)
