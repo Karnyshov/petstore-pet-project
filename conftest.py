28b05bf4855342f965663d3d2d1d23930e6162dc
Kyrylo Karnyshov <kyrylo.karnyshov@gmail.com>
2024-12-30 22:48:40 +0200

    Pet tests (#2)
    * Added tests for status codes.
    Added basic creation of invalid order in order.py
    
    * Added first tests for status codes based on Store Service
    Added basic creation of pet in pet.py
    
    * Added basic tests for status codes for endpoints that support JSON.
    POST request for image upload and pet update by form data have been omitted.

--- conftest.py
+++ conftest.py
@@ -1,7 +1,13 @@
 import pytest
 from src.services.store import StoreService
+from src.services.pet import PetService
 
 
 @pytest.fixture
 def store_service():
     yield StoreService()
+
+
+@pytest.fixture
+def pet_service():
+    yield PetService()
---
+++ src/core/objects/pet.py
@@ -0,0 +1,129 @@
+from dataclasses import dataclass
+from typing import Any
+from random import randint
+import json
+
+
+@dataclass
+class Pet:
+    pet_id: Any
+    name: str
+    status: str
+    photoUrls: list
+    tags: list
+    category: dict
+
+    @classmethod
+    def generate_pet(cls):
+        cls.pet_id = randint(0, 10)
+        cls.name = "Bob"
+        cls.status = "available"
+        cls.photoUrls = ["https://youtube.com"]
+        cls.tags = [{
+            "id": randint(0, 10),
+            "name": "Tag Test"
+        }]
+        cls.category = {
+            "id": randint(0, 10),
+            "name": "dog"
+        }
+
+        # TODO: move as function
+        pet_json = {
+            "id": cls.pet_id,
+            "name": cls.name,
+            "status": cls.status,
+            "photoUrls": cls.photoUrls,
+            "tags": cls.tags,
+            "category": cls.category
+        }
+
+        return json.dumps(pet_json)
+
+    @classmethod
+    def generate_updated_pet(cls, created_pet):
+        parsed_created_pet = json.loads(created_pet)
+        created_pet_id = parsed_created_pet.get("id")
+
+        cls.pet_id = created_pet_id
+        cls.name = "Bob_updated"
+        cls.status = "sold"
+        cls.photoUrls = ["https://youtube.com"]
+        cls.tags = [{
+            "id": randint(0, 10),
+            "name": "Tag Test Updated"
+        }]
+        cls.category = {
+            "id": randint(0, 10),
+            "name": "dog updated"
+        }
+
+        # TODO: move as function
+        pet_json = {
+            "id": cls.pet_id,
+            "name": cls.name,
+            "status": cls.status,
+            "photoUrls": cls.photoUrls,
+            "tags": cls.tags,
+            "category": cls.category
+        }
+
+        return json.dumps(pet_json)
+
+
+    @classmethod
+    def generate_updated_pet_invalid_id(cls):
+        cls.pet_id = "qwe"
+        cls.name = "Bob_updated"
+        cls.status = "sold"
+        cls.photoUrls = ["https://youtube.com"]
+        cls.tags = [{
+            "id": randint(0, 10),
+            "name": "Tag Test Updated"
+        }]
+        cls.category = {
+            "id": randint(0, 10),
+            "name": "dog updated"
+        }
+
+        # TODO: move as function
+        pet_json = {
+            "id": cls.pet_id,
+            "name": cls.name,
+            "status": cls.status,
+            "photoUrls": cls.photoUrls,
+            "tags": cls.tags,
+            "category": cls.category
+        }
+
+        return json.dumps(pet_json)
+
+    @classmethod
+    def generate_updated_pet_invalid_body(cls, created_pet):
+        parsed_created_pet = json.loads(created_pet)
+        created_pet_id = parsed_created_pet.get("id")
+
+        cls.pet_id = created_pet_id
+        cls.name = "Bob_updated"
+        cls.status = "sold"
+        cls.photoUrls = ["https://youtube.com"]
+        cls.tags = [{
+            "id": "qwe",
+            "name": "Tag Test Updated"
+        }]
+        cls.category = {
+            "id": randint(0, 10),
+            "name": "dog updated"
+        }
+
+        # TODO: move as function
+        pet_json = {
+            "id": cls.pet_id,
+            "name": cls.name,
+            "status": cls.status,
+            "photoUrls": cls.photoUrls,
+            "tags": cls.tags,
+            "category": cls.category
+        }
+
+        return json.dumps(pet_json)
