533a6af6401d0b4933fa7d18da888d855e2c8b3c
Kyrylo Karnyshov <kyrylo.karnyshov@gmail.com>
2025-03-31 19:53:57 +0300

    First attempt of logging implementation.

--- src/services/store.py
+++ src/services/store.py
@@ -1,5 +1,6 @@
 from src.core.baseAPI import BaseAPI
 from requests.api import get, post, delete
+import logging
 
 
 class StoreService:
@@ -8,17 +9,30 @@ class StoreService:
 
     headers = BaseAPI.headers
 
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filename="store.log",
+        encoding="utf-8"
+    )
+
     def get_inventory(self):
+        logging.info(f"sending GET request to {self.inventory_url}")
         return get(url=self.inventory_url, headers=self.headers)
 
     def get_order(self, order_id):
+        logging.info(f"sending GET request to {self.store_url} with ID: {order_id}")
         return get(url=self.store_url + f"{order_id}", headers=self.headers)
 
     def create_order(self, order):
+        logging.info(f"sending POST request to {self.store_url} with data: {order}")
         return post(url=self.store_url, data=order, headers=self.headers)
 
     def create_invalid_order(self, invalid_order):
+        logging.info(f"sending POST request to {self.store_url} with data: {invalid_order}")
         return post(url=self.store_url, data=invalid_order, headers=self.headers)
 
     def delete_order(self, order_id):
+        logging.info(f"sending DELETE request to {self.store_url} with ID: {order_id}")
         return delete(url=self.store_url + f"{order_id}", headers=self.headers)
