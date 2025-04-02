ac11187105c847e913f3db689c2a598ab25fb7ce
vmred <16134967+vmred@users.noreply.github.com>
2025-04-02 23:15:32 +0300

    feature - use global logger to log events to stdout / file (#10)

--- src/services/user.py
+++ src/services/user.py
@@ -3,7 +3,9 @@ import json
 from src.core.baseAPI import BaseAPI
 from src.core.objects.user import User
 from requests.api import get, post, put, delete
-import logging
+
+from utils.logger import logger
+
 
 class UserService:
     user_url = f"{BaseAPI.base_url}user/"
@@ -24,52 +26,52 @@ class UserService:
         return data
 
     def get_user(self, username):
-        logging.info(f"GET request to {self.user_url} with username: {username}")
+        logger.info(f"GET request to {self.user_url} with username: {username}")
         return get(url=self.user_url + f"{username}", headers=self.headers)
 
     def create_user(self, user):
-        logging.info(f"POST request to {self.user_url} with data: {self.filter_sensitive_data(user)}")
+        logger.info(f"POST request to {self.user_url} with data: {self.filter_sensitive_data(user)}")
         return post(url=self.user_url, data=user, headers=self.headers)
 
     def update_user(self, username, user_updated):
-        logging.info(f"PUT request to {self.user_url}{username} with data: {user_updated}")
+        logger.info(f"PUT request to {self.user_url}{username} with data: {user_updated}")
         return put(url=self.user_url + f"{username}", data=user_updated, headers=self.headers)
 
     def delete_user(self, username):
-        logging.info(f"DELETE request to {self.user_url}{username}")
+        logger.info(f"DELETE request to {self.user_url}{username}")
         return delete(url=self.user_url + f"{username}", headers=self.headers)
 
     def login_user(self, username, password):
-        logging.info(f"GET request to login: {self.login_url} with username: {username}")
+        logger.info(f"GET request to login: {self.login_url} with username: {username}")
         return get(url=self.login_url + f"?username={username}&password={password}", headers=self.headers)
 
     def logout_user(self):
-        logging.info(f"GET request to logout: {self.logout_url}")
+        logger.info(f"GET request to logout: {self.logout_url}")
         return get(url=self.logout_url, headers=self.headers)
 
     #TODO: implement tests
     def create_user_with_list(self, user_list):
-        logging.info(f"POST creating users using list with URL: {self.user_url} and data: {user_list}")
+        logger.info(f"POST creating users using list with URL: {self.user_url} and data: {user_list}")
         return post(url=self.user_url, data=user_list, headers=self.headers)
 
     #TODO: implement tests
     def create_user_with_array(self, user_array):
-        logging.info(f"POST creating users using array with URL: {self.user_url} and data: {user_array}")
+        logger.info(f"POST creating users using array with URL: {self.user_url} and data: {user_array}")
         return post(url=self.user_url, data=user_array, headers=self.headers)
 
     @staticmethod
     def updating_user(created_user):
-        logging.info(f"updating user: {created_user.username}")
+        logger.info(f"updating user: {created_user.username}")
         return User(username=created_user.username)
 
     @staticmethod
     def updating_users(created_user, user):
-        logging.info(f"valid case, updating user: {created_user} with data: {user}")
+        logger.info(f"valid case, updating user: {created_user} with data: {user}")
         user.username = created_user
         return user
 
     @staticmethod
     def invalid_update_user(created_user, user):
-        logging.info(f"invalid case, updating user: {created_user} with data: {user}")
+        logger.info(f"invalid case, updating user: {created_user} with data: {user}")
         user.username = created_user.username
         return user
--- src/test_data/test_data_generation.py
+++ src/test_data/test_data_generation.py
@@ -1,3 +1,4 @@
+import logging
 import random
 from src.core.objects.order import Order
 from src.core.objects.pet import Pet
@@ -5,19 +6,34 @@ from src.core.objects.user import User
 from faker import Faker
 from random import randint, uniform, choices
 import string
-import logging
+
 
 fake = Faker()
 
-def random_int(minimal = 1, maximum = 1000):
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler()  # Ensure logs are output to console
+handler.setLevel(logging.DEBUG)
+
+if not logger.handlers:  # Prevent duplicate logs
+    logger.addHandler(handler)
+
+logger.propagate = False  # Ensure pytest does not block the logs
+
+
+def random_int(minimal=1, maximum=1000):
     return randint(minimal, maximum)
 
-def random_float(minimal = 1.0, maximum = 1000.0):
+
+def random_float(minimal=1.0, maximum=1000.0):
     return uniform(minimal, maximum)
 
-def random_string(length = 10):
+
+def random_string(length=10):
     rand_str = ''.join(choices(string.ascii_letters + string.digits, k=length))
     return rand_str
+
 
 def auto_generate_orders(order_test_data):
     logging.info("Generating list of Order test data")
@@ -37,8 +53,9 @@ def auto_generate_orders(order_test_data):
 
     return auto_generated_orders
 
+
 def generate_invalid_order(test_data):
-    logging.info("Generating invalid Order test data")
+    logger.info("Generating invalid Order test data")
     return Order(
         order_id=random.choice(test_data.get("id")),
         pet_id=random.choice(test_data.get("pet_id")),
@@ -48,8 +65,9 @@ def generate_invalid_order(test_data):
         complete=random.choice(test_data.get("complete"))
     )
 
+
 def auto_generate_pets(pet_test_data):
-    logging.info("Generating list of Pet test data")
+    logger.info("Generating list of Pet test data")
     auto_generated_pets = []
 
     for test_data in pet_test_data:
@@ -57,17 +75,18 @@ def auto_generate_pets(pet_test_data):
             Pet(
                 name=test_data.get("name"),
                 photoUrls=test_data.get("photo_urls"),
-                pet_id = test_data.get("id"),
+                pet_id=test_data.get("id"),
                 category=test_data.get("category"),
-                tags = test_data.get("tags"),
-                status = test_data.get("status")
+                tags=test_data.get("tags"),
+                status=test_data.get("status")
             )
         )
 
     return auto_generated_pets
 
+
 def generate_updated_pets(pet_test_data, petId=None):
-    logging.info("Generating list of updated Pet test data")
+    logger.info("Generating list of updated Pet test data")
     auto_generated_updated_pets = []
 
     for test_data in pet_test_data:
@@ -75,17 +94,18 @@ def generate_updated_pets(pet_test_data, petId=None):
             Pet(
                 name=test_data.get("name"),
                 photoUrls=test_data.get("photo_urls"),
-                pet_id = petId,
+                pet_id=petId,
                 category=test_data.get("category"),
-                tags = test_data.get("tags"),
-                status = test_data.get("status")
+                tags=test_data.get("tags"),
+                status=test_data.get("status")
             )
         )
 
     return auto_generated_updated_pets
 
+
 def generate_invalid_pet(test_data):
-    logging.info("Generating invalid Pet test data")
+    logger.info("Generating invalid Pet test data")
     return Pet(
         name=random.choice(test_data.get("name")),
         photoUrls=random.choice(test_data.get("photo_urls")),
@@ -95,19 +115,22 @@ def generate_invalid_pet(test_data):
         status=random.choice(test_data.get("status"))
     )
 
+
 def generate_form_data_pet(pet_status, name):
-    logging.info(f"Generating valid random data for form-data request: {name}, {pet_status}")
+    logger.info(f"Generating valid random data for form-data request: {name}, {pet_status}")
     return {"name": name, "status": pet_status}
 
+
 def generate_invalid_form_data_pet(invalid_form_data, pet_status, name):
-    logging.info(f"Generating invalid random data for form-data request: {name}, {pet_status}")
+    logger.info(f"Generating invalid random data for form-data request: {name}, {pet_status}")
     return {
         random.choice(invalid_form_data.get("name")): name,
         random.choice(invalid_form_data.get("status")): pet_status
     }
 
+
 def auto_generate_users(user_test_data):
-    logging.info("Generating list of User test data")
+    logger.debug("Generating list of User test data")
     auto_generated_users = []
 
     for test_data in user_test_data:
@@ -126,14 +149,15 @@ def auto_generate_users(user_test_data):
 
     return auto_generated_users
 
+
 def auto_generate_updated_users(user_test_data, username=None):
-    logging.info("Generating list of updated User test data")
+    logger.info("Generating list of updated User test data")
     auto_generated_updated_users = []
 
     for test_data in user_test_data:
         auto_generated_updated_users.append(
             User(
-                user_id = test_data.get("id"),
+                user_id=test_data.get("id"),
                 username=username,
                 firstName=test_data.get("first_name"),
                 lastName=test_data.get("last_name"),
@@ -146,8 +170,9 @@ def auto_generate_updated_users(user_test_data, username=None):
 
     return auto_generated_updated_users
 
+
 def generate_invalid_user(test_data):
-    logging.info("Generating invalid User test data")
+    logger.info("Generating invalid User test data")
     return User(
         user_id=random.choice(test_data.get("id")),
         username=random.choice(test_data.get("username")),
